633d2a5ef6bae0fe48cb50bdeb8a75a39639d514
A. Peixinho <a_peixinho@sapo.pt>
2023-04-14 16:05:39 +0100

    Initial Skeletons for Backend and Frontend

---
+++ backend/src/main/java/org/labseq/labseqapi/LabseqApiApplication.java
@@ -0,0 +1,13 @@
+package org.labseq.labseqapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabseqApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabseqApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/labseq/labseqapi/LabseqApiApplicationTests.java
@@ -0,0 +1,15 @@
+package org.labseq.labseqapi;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabseqApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
